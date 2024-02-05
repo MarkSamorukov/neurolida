60338e3ab11c53fbec5bb32f3e344f32c52ed0e3
Марк <m.a.samorukov.c@gmail.com>
2024-02-06 02:48:43 +0300

    main

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import telebot
+from telebot import types
+
+bot = telebot.TeleBot('6975528056:AAF4pgiATC0a4wmKFhH152azLzojupfpj8c')
 
 
-def main():
-    pass
+@bot.message_handler(content_types=['text'])
+def echo_all(message):
+    bot.send_message(message.chat.id, message.text)
 
 
 if __name__ == "__main__":
-    main()
+    bot.infinity_polling()
